ffa3d0a0d2319a4427c4e3819803cc16af2857df
Daniel Lai <laidaniel759@gmail.com>
2026-02-02 15:56:14 +0800

    update icon

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,13 @@ import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// Set custom favicon
+const faviconLink = document.createElement('link');
+faviconLink.rel = 'icon';
+faviconLink.type = 'image/svg+xml';
+faviconLink.href = new URL('./src/assets/tempah-now-logo.svg', window.location.origin).href;
+document.head.appendChild(faviconLink);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
